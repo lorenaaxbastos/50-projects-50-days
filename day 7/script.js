b7a500ca9913d0fd34e6140eccf8642e98576f92
Lorena Bastos <lorenaax.bastos@hotmail.com>
2025-02-04 19:31:56 -0300

    day 7 - split screen

--- day 7/script.js
+++ day 7/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const heroHeading = document.querySelector(".hero__heading");
+const teamHeadings = document.querySelectorAll(".heading--primary");
+const teamSides = document.querySelectorAll(".hero__side");
+
+function toggleSelection(clickedSide) {
+    teamSides.forEach((side) => {
+        if (side === clickedSide) {
+            side.classList.remove("hero__side--covered");
+        } else {
+            side.classList.add("hero__side--grayed-out");
+        }
+    });
+
+    heroHeading.classList.add("invisible");
+}
+
+teamHeadings.forEach((heading) => {
+    heading.addEventListener("click", function () {
+        const clickedSide = this.closest(".hero__side");
+        toggleSelection(clickedSide);
+    });
+});
