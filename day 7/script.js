7875e7c5bac3e0b3bbc5bcec28e24cfc94d5c15a
Lorena Bastos <lorenaax.bastos@hotmail.com>
2025-02-05 14:41:31 -0300

    day 7 - split screen

--- day 7/script.js
+++ day 7/script.js
@@ -3,22 +3,44 @@
 const heroHeading = document.querySelector(".hero__heading");
 const teamHeadings = document.querySelectorAll(".heading--primary");
 const teamSides = document.querySelectorAll(".hero__side");
+const btnChangeTeam = document.querySelectorAll(".btn");
 
-function toggleSelection(clickedSide) {
+const expandSide = function (clickedSide, eOrigin) {
+    const isExpanded = !clickedSide.classList.contains("hero__side--covered");
+    if (isExpanded && eOrigin.className === "heading--primary") return;
+
     teamSides.forEach((side) => {
         if (side === clickedSide) {
-            side.classList.remove("hero__side--covered");
+            side.classList.toggle("hero__side--covered");
         } else {
-            side.classList.add("hero__side--grayed-out");
+            side.classList.toggle("hero__side--grayed-out");
         }
     });
 
-    heroHeading.classList.add("invisible");
-}
+    heroHeading.classList.toggle("invisible");
+};
+
+const adjustTransitions = function (el, className) {
+    el.classList.add(className);
+    setTimeout(() => {
+        el.classList.remove(className);
+    }, 1000);
+};
 
 teamHeadings.forEach((heading) => {
     heading.addEventListener("click", function () {
         const clickedSide = this.closest(".hero__side");
-        toggleSelection(clickedSide);
+        expandSide(clickedSide, heading);
+    });
+});
+
+btnChangeTeam.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        const text = this.previousElementSibling;
+        const clickedSide = this.closest(".hero__side");
+
+        expandSide(clickedSide, btn);
+        adjustTransitions(btn, "btn--fast-transition");
+        adjustTransitions(text, "text--fast-transition");
     });
 });
